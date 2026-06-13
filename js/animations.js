3f1005ffd49b064a51d5c1a67728424dd55a83e6
Anamol Dhakal <dhakal.anamol.28@gmail.com>
2026-06-13 18:07:42 +0545

    fix: use rootMargin-based intersection for active nav link to support variable section heights

--- js/animations.js
+++ js/animations.js
@@ -24,7 +24,8 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 }, { 
-    threshold: 0.5 
+    rootMargin: '-50% 0px -50% 0px',
+    threshold: 0
 });
 
 sections.forEach(section => observer.observe(section));
